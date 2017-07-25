ca1e2844b168d5b79a617b8cba04bcb19bfa3041
sfitzgerald16 <misfitz616@gmail.com>
2017-07-24 22:01:43 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
+import { CribsService } from './services/cribs.service';
+import { AddListingFormComponent } from './add-listing-form/add-listing-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CribListingComponent,
+    CribCardComponent,
+    AddListingFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [CribsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/crib-listing/crib-listing.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { CribsService } from './../services/cribs.service';
+
+@Component({
+  selector: 'app-crib-listing',
+  templateUrl: './crib-listing.component.html',
+})
+export class CribListingComponent implements OnInit {
+
+  cribs: Array<any>;
+  error: string;
+
+  constructor(private http: Http, private cribsService: CribsService) { }
+
+  ngOnInit() {
+    this.cribsService.getAllCribs()
+      .subscribe(
+        data => this.cribs = data,
+        error => this.error = error.statusText
+      );
+
+    this.cribsService.newCribSubject.subscribe(
+      data => this.cribs = [data, ...this.cribs]
+    )
+  }
+
+}
