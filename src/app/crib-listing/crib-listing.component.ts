a039e70f1b235ddd6eef086efd3dec4c2f98a24f
sfitzgerald16 <misfitz616@gmail.com>
2017-07-25 21:10:34 -0700

    "finished"

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { CribsService } from './../services/cribs.service';
+import { UtilService } from './../services/util.service';
 
 @Component({
   selector: 'app-crib-listing',
@@ -10,8 +11,22 @@ export class CribListingComponent implements OnInit {
 
   cribs: Array<any>;
   error: string;
+  sortField: string = 'price';
+  sortDirection: string = 'asc';
+  sortFields: Array<string> = [
+    'address',
+    'area',
+    'bathrooms',
+    'bedrooms',
+    'price',
+    'type'
+  ]
 
-  constructor(private http: Http, private cribsService: CribsService) { }
+  constructor(
+    private http: Http,
+    private cribsService: CribsService,
+    private utilService: UtilService
+  ) { }
 
   ngOnInit() {
     this.cribsService.getAllCribs()
